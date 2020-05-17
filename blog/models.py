a62b30dc60fb10052cec3cc7596f7d0c8671da61
unknown <israelcarneiro97@gmail.com>
2020-05-17 04:30:58 -0300

    organizando pastas e css

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='publicado')
+
 class Post(models.Model):
     STATUS = (
         ('rascunho','Rascunho'),
@@ -18,7 +23,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS,
                               default='rascunho')
-
+    objects = models.Manager()
+    published = PublishedManager()
     class Meta:
         ordering = ('-publicado',)
 
