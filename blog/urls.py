8d48db31ae67ffd8111b4f977bebf1d9da5bd0f8
unknown <israelcarneiro97@gmail.com>
2020-05-20 20:20:56 -0300

    Criar e editar posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/edit/<int:pk>', views.BlogUpdateView.as_view(), name='post_edit'),
     path('post/<slug:slug>/', views.BlogDetailView.as_view(), name='post_detail'),
 ]
