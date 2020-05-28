541c35fa6a7ebc4b0f7091fb54614f28595c7083
unknown <israelcarneiro97@gmail.com>
2020-05-28 04:40:03 -0300

    Tela de cadastro de usuário e verificação de email

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserCreationFormWithEmail(UserCreationForm):
+    email = forms.EmailField(required=True, help_text='Obrigatório, digite um email válido!')
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este email já está cadastrado. Utilize outro, por favor.")
+        return email
+
